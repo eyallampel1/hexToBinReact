f031f2716b9fccd18b635093b0bba04f23d2dc86
Eyal Lampel <eyal.lampel@gmail.com>
2023-07-06 23:15:00 +0300

    added the backend and functionally to the code , made it cleaner and more efficient

--- src/App.js
+++ src/App.js
@@ -1,57 +1,70 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
 import HexToBin from './component/HexToBin';
-import { useState } from 'react';
+import './App.css';
+
+const BIT_MODES = {
+  SHORT: 4,  // For 16-bit mode
+  LONG: 8    // For 32-bit mode
+}
 
 function App() {
-  const [LongHex, setLongHex] = useState(false);
+  // Initialize the application in 16-bit mode
+  const [bitMode, setBitMode] = useState(BIT_MODES.SHORT);
 
+  // Function to change the bit mode
+  const handleBitModeChange = event => {
+    setBitMode(event.target.value === "16bit" ? BIT_MODES.SHORT : BIT_MODES.LONG);
+  };
+
+  // Generate the HexToBin components based on the current bit mode
+  const hexToBinComponents = Array(bitMode).fill().map((_, i) => (
+    <HexToBin key={i} className="forMarginName" />
+  ));
+
+  // Generate the binary index numbers
+  const binIndexNumbers = Array(bitMode).fill().map((_, i) => (
+    <div key={i} className="binIndexNumber">{i + 1}</div>
+  ));
 
   return (
     <React.Fragment>
       <h1>Hex to Binary Convertor</h1>
       <div className="MainApp">
-
-        {LongHex ? (
-          <div className="hex2binComp">
-            <HexToBin />
-            <HexToBin />
-            <HexToBin />
-            <HexToBin />
-          </div>
-        ) : (
-          <div className="hex2binComp">
-            <HexToBin />
-            <HexToBin />
-            <HexToBin />
-            <HexToBin />
-            <HexToBin />
-            <HexToBin />
-            <HexToBin />
-            <HexToBin />
-          </div>
-        )}
-
-
+        <div className="hex2binComp">
+          {hexToBinComponents}
+        </div>
+      </div>
+      <div className="binIndexNumberContainer">
+        {binIndexNumbers}
       </div>
       <fieldset className='radio_container'>
         <legend>Choose length :</legend>
         <div>
-          <input className='radio_input' type="radio" id="16bit" name="drone" value="16bit" defaultChecked
-            onClick={() => { setLongHex(true) }} />
+          <input
+            className='radio_input'
+            type="radio"
+            id="16bit"
+            name="drone"
+            value="16bit"
+            defaultChecked
+            onClick={handleBitModeChange}
+          />
           <label className='radio_label'>16bit</label>
         </div>
         <div>
-          <input className='radio_input' type="radio" id="32bit" name="drone" value="32bit" onClick={() => { setLongHex(false) }} />
+          <input
+            className='radio_input'
+            type="radio"
+            id="32bit"
+            name="drone"
+            value="32bit"
+            onClick={handleBitModeChange}
+          />
           <label className='radio_label'>32bit</label>
         </div>
-
-
       </fieldset>
     </React.Fragment>
   );
 }
 
 export default App;
-
-
--- src/component/HexToBin.js
+++ src/component/HexToBin.js
@@ -1,195 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './HexToBin.css';
-import { useState } from 'react';
-
-// let bin0 = 0;
-// let bin1 = 0;
-// let bin2 = 0;
-// let bin3 = 0;
-
 
 const HexToBin = () => {
+  const [bin, setBin] = useState([0, 0, 0, 0]);
+  const [hexValue, setHexValue] = useState('');
 
-  //define onstate named bin0
-  const [bin0, setBin0] = useState(0);
-  //define onstate named bin1
-  const [bin1, setBin1] = useState(0);
-  //define onstate named bin2
-  const [bin2, setBin2] = useState(0);
-  //define onstate named bin3
-  const [bin3, setBin3] = useState(0);
+  const hexToBinMap = {
+    '0': [0, 0, 0, 0], '1': [0, 0, 0, 1], '2': [0, 0, 1, 0], '3': [0, 0, 1, 1],
+    '4': [0, 1, 0, 0], '5': [0, 1, 0, 1], '6': [0, 1, 1, 0], '7': [0, 1, 1, 1],
+    '8': [1, 0, 0, 0], '9': [1, 0, 0, 1], 'a': [1, 0, 1, 0], 'b': [1, 0, 1, 1],
+    'c': [1, 1, 0, 0], 'd': [1, 1, 0, 1], 'e': [1, 1, 1, 0], 'f': [1, 1, 1, 1]
+  };
 
-  const [hexValue, setHexValue] = useState('0');
+  const binToHexMap = {
+    '0000': '0', '0001': '1', '0010': '2', '0011': '3',
+    '0100': '4', '0101': '5', '0110': '6', '0111': '7',
+    '1000': '8', '1001': '9', '1010': 'a', '1011': 'b',
+    '1100': 'c', '1101': 'd', '1110': 'e', '1111': 'f'
+  };
 
-  const [validHexInput, setValidHexInput] = useState(true);
+  const convertToBin = (event) => {
+    let input = event.target.value.toLowerCase();
 
-
-
-  //write arrow function named ConvertToBin and send to it the input value
-  const ConvertToBin = (event) => {
-    //convert the input value to binary
-    //let input = inputRef.current.value;
-    let input = event.target.value;
-
-    let binary = parseInt(input, 16).toString(2);
-    //if input is equal to NAN then set all the binary to 0
-    // if (!isNaN(input)) {
-    //   console.log("inside NAN");
-    //   setBin3(0);
-    //   setBin2(0);
-    //   setBin1(0);
-    //   setBin0(0);
-    // }
-
-     if (input === 'a') {
-      console.log("inside a");
-      setBin3(1);
-      setBin2(0);
-      setBin1(1);
-      setBin0(0);
+    if (hexToBinMap.hasOwnProperty(input)) {
+      setBin(hexToBinMap[input]);
+      setHexValue(input);
+    } else {
+      setBin([0, 0, 0, 0]);
+      setHexValue('');
     }
-    else if (input === 'b') {
-      console.log("inside b");
-      setBin3(1);
-      setBin2(0);
-      setBin1(1);
-      setBin0(1);
-    }
-    else if (input === 'c') {
-      console.log("inside c");
-      setBin3(1);
-      setBin2(1);
-      setBin1(0);
-      setBin0(0);
-    }
-    else if (input === 'd') {
-      console.log("inside d");
-      setBin3(1);
-      setBin2(1);
-      setBin1(0);
-      setBin0(1);
-    }
-    else if (input === 'e') {
-      console.log("inside e");
-      setBin3(1);
-      setBin2(1);
-      setBin1(1);
-      setBin0(0);
-    }
-    else if (input === 'f') {
-      console.log("inside f");
-      setBin3(1);
-      setBin2(1);
-      setBin1(1);
-      setBin0(1);
-    }
-    else if (input === '0') {
-      console.log("inside 0");
-      setBin3(0);
-      setBin2(0);
-      setBin1(0);
-      setBin0(0);
-    }
-    else if (input === '1') {
-      console.log("inside 1");
-      setBin3(0);
-      setBin2(0);
-      setBin1(0);
-      setBin0(1);
-    }
-    else if (input === '2') {
-      console.log("inside 2");
-      setBin3(0);
-      setBin2(0);
-      setBin1(1);
-      setBin0(0);
-    }
-    else if (input === '3') {
-      console.log("inside 3");
-      setBin3(0);
-      setBin2(0);
-      setBin1(1);
-      setBin0(1);
-    }
-    else if (input === '4') {
-      console.log("inside 4");
-      setBin3(0);
-      setBin2(1);
-      setBin1(0);
-      setBin0(0);
-    }
-    else if (input === '5') {
-      console.log("inside 5");
-      setBin3(0);
-      setBin2(1);
-      setBin1(0);
-      setBin0(1);
-    }
-    else if (input === '6') {
-      console.log("inside 6");
-      setBin3(0);
-      setBin2(1);
-      setBin1(1);
-      setBin0(0);
-    }
-    else if (input === '7') {
-      console.log("inside 7");
-      setBin3(0);
-      setBin2(1);
-      setBin1(1);
-      setBin0(1);
-    }
-    else if (input === '8') {
-      console.log("inside 8");
-      setBin3(1);
-      setBin2(0);
-      setBin1(0);
-      setBin0(0);
-    }
-    else if (input === '9') {
-      console.log("inside 9");
-      setBin3(1);
-      setBin2(0);
-      setBin1(0);
-      setBin0(1);
-      //setHexValue('9');
-      setValidHexInput(false);
-      console.log(event.target.value);
-    }
-    else {
-      console.log("inside else");
-      setBin3(0);
-      setBin2(0);
-      setBin1(0);
-      setBin0(0);
-      console.log( validHexInput);
-      event.target.value = ''
-      setValidHexInput(true);
-      
-      
-    }
-
-    //return the binary value
-    console.log(input);
-    console.log(binary);
-    return binary;
   }
 
-  //get the value of the input 
-
+  const toggleBit = (index) => {
+    setBin(prevBin => {
+      let newBin = [...prevBin];
+      newBin[index] = newBin[index] === 0 ? 1 : 0;
+      let newHexValue = binToHexMap[newBin.join('')];
+      setHexValue(newHexValue);
+      return newBin;
+    });
+  }
 
   return (
     <div className="byte_container">
-      <input type="text" className='HexInput'  onChange={ConvertToBin} maxlength="1" pattern="[a-fA-F0-9]{1}" required ></input>
+      <input
+        type="text"
+        className='HexInput'
+        value={hexValue}
+        onChange={convertToBin}
+        maxLength="1"
+        pattern="[a-fA-F0-9]{1}"
+        required
+      />
       <div className="btn_container">
-        <button>{bin3}</button>
-        <button>{bin2}</button>
-        <button>{bin1}</button>
-        <button>{bin0}</button>
+        <button onClick={() => toggleBit(0)}>{bin[0]}</button>
+        <button onClick={() => toggleBit(1)}>{bin[1]}</button>
+        <button onClick={() => toggleBit(2)}>{bin[2]}</button>
+        <button onClick={() => toggleBit(3)}>{bin[3]}</button>
       </div>
-    </div >
-  )
+    </div>
+  );
 }
 
 export default HexToBin;
-
